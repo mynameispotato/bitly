90c1816828fe505fdf75d5c5773e872e70c3ca19
c <mksim.byzov@mail.ru>
2022-06-27 20:59:31 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import os
+import requests
+from urllib.parse import urlparse
+from dotenv import load_dotenv
+import argparse
+load_dotenv()
+
+BITLY_TOKEN = os.environ['BITLY_TOKEN']
+TEMPLATE_LINK = "https://api-ssl.bitly.com/v4/bitlinks/"
+
+
+def shorter_link(link, headers):
+    short_link = "https://api-ssl.bitly.com/v4/shorten"
+    payload = {"long_url": link}
+    response = requests.post(short_link, json=payload, headers=headers)
+    response.raise_for_status()
+    return response.json()["id"]
+
+
+def count_clicks(link, headers):
+    clicks_count_link = TEMPLATE_LINK + f"{link}/clicks/summary"
+    response = requests.get(clicks_count_link, headers=headers)
+    response.raise_for_status()
+    return response.json()["total_clicks"]
+
+
+def is_bitlink(url, headers):
+    parse_link = urlparse(url)
+    link_bitly = TEMPLATE_LINK + f"{parse_link.netloc}{parse_link.path}"
+    response = requests.get(link_bitly, headers=headers)
+
+    return response.ok
+
+
+def main():
+    headers = {'Authorization': 'Bearer {}'.format(BITLY_TOKEN)}
+    link_to_short = argparse.ArgumentParser(
+    description='Сокращает ссылку/проверяет кол-во переходов по сокращённой ссылке'
+    )
+    link_to_short.add_argument('--url', help='ссылка')
+    args = link_to_short.parse_args()
+    link_to_short = args.url
+
+    try:
+        if is_bitlink(link_to_short, headers):
+            click_count = count_clicks(link_to_short, headers)
+            print("Кол-во переходов по ней: ", click_count)
+
+        else:
+            bitly_link = shorter_link(link_to_short, headers)
+            print("Сокращённая ссылка: ", bitly_link)
+
+    except requests.exceptions.HTTPError as error:
+        print("Can't get data from server:\n{0}".format(error))
+
+if __name__ == "__main__":
+    main()
