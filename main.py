7665e703d8629600e8d99c489e684ec6b96fb219
c <mksim.byzov@mail.ru>
2022-07-09 18:04:56 +0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import argparse
 TEMPLATE_API_LINK = "https://api-ssl.bitly.com/v4/bitlinks"
 
 
-def link_shorter(link, headers):
+def shorter_link(link, headers):
     short_link = "https://api-ssl.bitly.com/v4/shorten"
     payload = {"long_url": link}
     response = requests.post(short_link, json=payload, headers=headers)
@@ -17,7 +17,7 @@ def link_shorter(link, headers):
 
 
 def count_clicks(link, headers):
-    clicks_count_link = f"{TEMPLATE_LINK}/{link}/clicks/summary"
+    clicks_count_link = f"{TEMPLATE_API_LINK}/{link}/clicks/summary"
     response = requests.get(clicks_count_link, headers=headers)
     response.raise_for_status()
     return response.json()["total_clicks"]
@@ -25,7 +25,7 @@ def count_clicks(link, headers):
 
 def is_bitlink(url, headers):
     parse_link = urlparse(url)
-    link_bitly = f"{TEMPLATE_LINK}/{parse_link.netloc}{parse_link.path}"
+    link_bitly = f"{TEMPLATE_API_LINK}/{parse_link.netloc}{parse_link.path}"
     response = requests.get(link_bitly, headers=headers)
 
     return response.ok
@@ -39,8 +39,8 @@ def main():
         description='''Сокращает ссылку/
         проверяет кол-во переходов по сокращённой ссылке'''
     )
-    function.add_argument('--url', help='ссылка')
-    args = function.parse_args()
+    parser.add_argument('--url', help='ссылка')
+    args = parser.parse_args()
 
     try:
         if is_bitlink(args.url, headers):
@@ -48,7 +48,7 @@ def main():
             print("Кол-во переходов по ней: ", click_count)
 
         else:
-            bitly_link = link_shorter(args.url, headers)
+            bitly_link = shorter_link(args.url, headers)
             print("Сокращённая ссылка: ", bitly_link)
 
     except requests.exceptions.HTTPError as error:
